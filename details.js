bf000b1b9ce4484ee14779583551c40de5e6eeee
Luke Leigh <luke.leigh@gmail.com>
2025-03-15 22:18:09 +0000

    Update details.js

--- details.js
+++ details.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const params = new URLSearchParams(window.location.search);
-    const src = params.get("src");
-    const desc = params.get("desc");
+    const imageSrc = params.get("id");
+    const description = params.get("desc");
 
-    if (src && desc) {
-        document.getElementById("detail-img").src = src;
-        document.getElementById("detail-desc").innerText = desc;
+    if (imageSrc && description) {
+        document.getElementById("detail-img").src = imageSrc;
+        document.getElementById("detail-desc").innerText = description;
     }
 });
